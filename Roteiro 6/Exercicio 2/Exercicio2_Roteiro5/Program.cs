28541df2d956277bb093cf1f270ef45f2a7a3907
Kewerton <80172116+kewertonn@users.noreply.github.com>
2025-04-14 22:25:26 -0300

    Add files via upload

---
+++ Roteiro 6/Exercicio 2/Exercicio2_Roteiro5/Program.cs
@@ -0,0 +1,28 @@
+﻿/**************************************************
+                 14/04/2025
+    Kewerton Caetano - RA 123210 - Roteiro 06
+                Exercicio 2 
+**************************************************/
+
+using System;
+
+class Program {
+static void Main() {
+    /*
+    Console.Write("Digite o nome do produto: ");
+    string nome = Console.ReadLine();
+    if(nome == ""){
+        throw new ArgumentException("Erro: Nome do produto NÃO pode ficar EM BRANCO!");
+    }
+
+    Console.Write("Digite o preço do produto: ");
+    int preco = int.Parse(Console.ReadLine());
+    if(preco <= 0){
+        throw new ArgumentException("Erro: O valor do produto deve ser maior que ZERO!");
+    }
+    
+    Console.WriteLine($"Nome: {nome}\n");
+    Console.WriteLine($"Preço: {preco}");
+    */
+}
+}

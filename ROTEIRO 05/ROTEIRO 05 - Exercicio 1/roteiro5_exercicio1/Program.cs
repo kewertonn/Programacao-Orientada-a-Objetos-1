7e52590a9f68ed545bb9c9a2d5d57e16e1197428
Kewerton <80172116+kewertonn@users.noreply.github.com>
2025-05-26 02:38:43 -0300

    Add files via upload

---
+++ ROTEIRO 05/ROTEIRO 05 - Exercicio 1/roteiro5_exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        decimal[] numeros = new decimal[5];
+
+        Console.WriteLine("Digite 5 números decimais:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Digite o {i + 1}º Número Decimal: ");
+            numeros[i] = decimal.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Número original: {numeros[i]}");
+            Console.WriteLine($"Arredondamento padrão (Math.Round): {Math.Round(numeros[i])}");
+            Console.WriteLine($"Arredondamento para baixo (Math.Floor): {Math.Floor(numeros[i])}");
+            Console.WriteLine($"Arredondamento para cima (Math.Ceiling): {Math.Ceiling(numeros[i])}");
+            Console.WriteLine($"Truncamento (Math.Truncate): {Math.Truncate(numeros[i])} \n");
+        }
+
+    }
+}
---
+++ ROTEIRO 05/ROTEIRO 05 - Exercicio 3/roteiro5_exercicio3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        
+        Console.Write("Digite a data e hora do compromisso (formato dd/MM/yyyy HH:mm): ");
+        string dataCompromisso = Console.ReadLine();
+
+        DateTime compromisso;
+        bool sucesso = DateTime.TryParseExact(
+            dataCompromisso,
+            "dd/MM/yyyy HH:mm",
+            CultureInfo.InvariantCulture,
+            DateTimeStyles.None,
+            out compromisso
+        );
+
+        if (!sucesso)
+        {
+            Console.WriteLine("Data e hora inválidas. Execute o programa novamente.");
+            return;
+        }
+
+        DateTime agora = DateTime.Now;
+        TimeSpan diferenca = compromisso.Date - agora.Date;
+        int diasFaltando = diferenca.Days;
+
+        
+        string diaSemana = compromisso.ToString("dddd", new CultureInfo("pt-BR"));
+
+        string mensagem = string.Format(
+            "Olá, {0}!\nSeu compromisso será em {1} dias, na {2}.\nData marcada: {3} às {4}",
+            nome,
+            diasFaltando >= 0 ? diasFaltando : 0,
+            diaSemana,
+            compromisso.ToString("dd/MM/yyyy"),
+            compromisso.ToString("HH:mm")
+        );
+
+        Console.WriteLine("\n" + mensagem);
+    }
+}
